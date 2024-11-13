eb347ae8f41d193ce305133fafcb94ee1840aa96
59date <andy6848@naver.com>
2024-11-13 16:44:38 +0900

    Initial Commit

---
+++ BMI_ljhTests/BMI_ljhTests.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_ljhTests.swift
+//  BMI_ljhTests
+//
+//  Created by LimJongHoon on 11/13/24.
+//
+
+import Testing
+@testable import BMI_ljh
+
+struct BMI_ljhTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
